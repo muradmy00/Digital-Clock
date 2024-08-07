3c443ed4dacdb6ea05255d4fa895e21638464277
muradmy00 <murad.my00@gmail.com>
2024-08-08 01:17:19 +0600

    project

---
+++ script.js
@@ -0,0 +1,17 @@
+function updateClock(){
+
+
+    const now = new Date();
+    let hours = now.getHours();
+    const meridiem = hours >=12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2,0);
+    const minutes = now.getMinutes().toString().padStart(2,0);
+    const second = now.getSeconds().toString().padStart(2,0);
+    const timeString = `${hours}:${minutes}:${second} ${meridiem}`;
+    document.getElementById("clock").innerText = timeString;
+
+}
+
+updateClock();
+setInterval(updateClock,1000);
